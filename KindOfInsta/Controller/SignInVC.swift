0d836001953ba0f4035c9c3f8615b0f942cb9975
Jakub Slawecki <Jake@MacBook-Pro-Jakub.local>
2018-03-14 11:17:08 +0100

    Completed setting up Firebase Integration

---
+++ KindOfInsta/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindOfInsta
+//
+//  Created by Jakub Slawecki on 14.03.2018.
+//  Copyright © 2018 Jakub Slawecki. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
