234b3325e65d3622312953fb1dc883563b67c6b3
Jakub Slawecki <jakubslawecki@gmail.com>
2018-03-26 09:49:05 +0200

    TableViews test cell

--- KindOfInsta/View/PostCell.swift
+++ KindOfInsta/View/PostCell.swift
@@ -13,7 +13,7 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var postImg: UIImageView!
-    @IBOutlet weak var caption: UILabel!
+    @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
     
     
